e484ed347baa2b0499aa3f8e41901574dcd8876c
mahmoud <mahmoudyaya33@gmail.com>
2026-03-17 10:30:58 +0100

    Add user page

---
+++ WORKTOGETHER.WPF/Views/UserWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using WORKTOGETHER.DATA.Repositories;
+using System.Windows.Shapes;
+
+namespace WORKTOGETHER.WPF.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour UserWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window
+    {
+        private readonly UserRepository _repository;
+
+        public UserWindow()
+        {
+            InitializeComponent();
+            _repository = new UserRepository();
+            ChargerUsers();
+        }
+
+        private void ChargerUsers()
+        {
+            DgUtilisateurs.ItemsSource = _repository.FindAll();
+        }
+
+
+    }
+
+}
